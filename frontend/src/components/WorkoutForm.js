0fc1a8fdfd6de33c1150b5520329667464c89644
kirthika <23009434@myrp.edu.sg>
2025-01-22 09:10:00 +0800

    now

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -5,9 +5,36 @@ const WorkoutForm = () => {
     const [title, setTitle] = useState('')
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
+    const [error, setError] = useState(null)
+
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+
+      const Workout = { title, load, reps }
+
+      const response = await fetch('/api/workouts', {
+        method: 'POST',
+        body: JSON.stringify(Workout),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const json = await response.json()
+
+      if (!response.ok) {
+         setError(json.error)
+      }
+      if (response.ok) {
+        setTitle('')
+        setLoad('')
+        setReps('')
+        setError(null)
+        console.log('new workout added',json)
+      }
+    }
 
     return (
-        <form className="create">
+        <form className="create" onSubmit={handleSubmit}> 
             <h3>Add a New Workout</h3>
 
             <label>Exercise Title:</label>
@@ -30,6 +57,10 @@ const WorkoutForm = () => {
               onChange={(e) => setReps(e.target.value)}
               value={reps}
             />
+            <button>Add Workout</button>
+            {error && <div className="error">{error}</div>}
         </form>
    )
 }
+
+export default WorkoutForm;
