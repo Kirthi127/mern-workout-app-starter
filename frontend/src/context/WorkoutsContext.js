6b6c7a7d6d449371059007bd7ed0997f6a3742f1
kirthika <23009434@myrp.edu.sg>
2025-01-23 10:53:14 +0800

    dis

--- frontend/src/context/WorkoutsContext.js
+++ frontend/src/context/WorkoutsContext.js
@@ -1,7 +1,16 @@
+// Centralized way to manage the state of the workouts
+
+// CreateContext: Create a context object.
+// useReducer: An alternative to useState. 
+// Accepts a reducer of type (state, action) => newState, 
+// and returns the current state paired with a dispatch method.
 import { createContext, useReducer } from 'react'
 
+// Context used to share the workout state and DISPATCH
+// function across the component tree.
 export const WorkoutsContext = createContext()
 
+// Reducer function to handle the state changes
 export const workoutsReducer = (state, action) => {
   switch (action.type) {
     case 'SET_WORKOUTS':
@@ -12,12 +21,18 @@ export const workoutsReducer = (state, action) => {
       return { 
         workouts: [action.payload, ...state.workouts] 
       }
+    case 'DELETE_WORKOUT':
+      return { 
+        workouts: state.workouts.filter(w => w._id !== action.payload._id) 
+      }
     default:
       return state
   }
 }
 
+// Context provider
 export const WorkoutsContextProvider = ({ children }) => {
+  // useReducer returns the current state and a dispatch method
   const [state, dispatch] = useReducer(workoutsReducer, { 
     workouts: null
   })
